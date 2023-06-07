916a68a231ae3309fe14f5b4cf50019380f6e4b2
kyinternreact2023 <kyinternreact2023@gmail.com>
2023-06-07 23:23:41 +0700

    set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' abc'}
+            By{' abcd'}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
